370d36b39310463aa4469d3ee248a296e95cf30c
luccaDD <lucca.debbio@perallis.com>
2023-05-31 00:41:14 -0300

    drastically changed direction, trying for a retro-futuristic vibe

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const {
+    default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette")
+
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
@@ -12,16 +16,40 @@ module.exports = {
                     dark: "#0f0c29",
                     light: "#302b63",
                 },
+                gray: {
+                    dark: "#2d2928",
+                    light: "#403a3a",
+                },
+                offwhite: "#e1d2ac",
+                blue: {
+                    accent: "#8bc4c2",
+                },
             },
         },
     },
     plugins: [
-        function ({ addComponents, theme }) {
+        function ({ addComponents, matchUtilities, theme }) {
             addComponents({
-                html: {
-                    fontFamily: "Quicksand",
+                ".ccontainer": {
+                    margin: "auto",
+                    padding: "1.5rem",
+                    maxWidth: "100ch",
                 },
-            })
+            }),
+                matchUtilities(
+                    {
+                        "text-glow-sm": (value) => ({
+                            "text-shadow": "0 0 5px " + value,
+                        }),
+                        "text-glow-md": (value) => ({
+                            "text-shadow": "0 0 15px " + value,
+                        }),
+                    },
+                    {
+                        values: flattenColorPalette(theme("colors")),
+                        type: "color",
+                    }
+                )
         },
     ],
 }
